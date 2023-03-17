aa34dfdf7561aee29ddf287c2a58939cf8d3fb22
Griegoriens <skwarnel@gmail.com>
2023-03-17 19:34:41 +0100

    Form-confirmation - added style.css

--- src/main/webapp/WEB-INF/views/form-confirmation.jsp
+++ src/main/webapp/WEB-INF/views/form-confirmation.jsp
@@ -1,3 +1,7 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <%--
   Created by IntelliJ IDEA.
   User: griegoriens
@@ -13,7 +17,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>Document</title>
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="<c:url value="resources/css/style.css"/>"/>
 </head>
 <body>
 <header class="header--form-page">
--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -1,203 +1,204 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
-  /**
-   * Form Select
-   */
-  class FormSelect {
-    constructor($el) {
-      this.$el = $el;
-      this.options = [...$el.children];
-      this.init();
-    }
-
-    init() {
-      this.createElements();
-      this.addEvents();
-      this.$el.parentElement.removeChild(this.$el);
-    }
-
-    createElements() {
-      // Input for value
-      this.valueInput = document.createElement("input");
-      this.valueInput.type = "text";
-      this.valueInput.name = this.$el.name;
-
-      // Dropdown container
-      this.dropdown = document.createElement("div");
-      this.dropdown.classList.add("dropdown");
-
-      // List container
-      this.ul = document.createElement("ul");
-
-      // All list options
-      this.options.forEach((el, i) => {
-        const li = document.createElement("li");
-        li.dataset.value = el.value;
-        li.innerText = el.innerText;
-
-        if (i === 0) {
-          // First clickable option
-          this.current = document.createElement("div");
-          this.current.innerText = el.innerText;
-          this.dropdown.appendChild(this.current);
-          this.valueInput.value = el.value;
-          li.classList.add("selected");
+    /**
+     * Form Select
+     */
+    class FormSelect {
+        constructor($el) {
+            this.$el = $el;
+            this.options = [...$el.children];
+            this.init();
         }
 
-        this.ul.appendChild(li);
-      });
-
-      this.dropdown.appendChild(this.ul);
-      this.dropdown.appendChild(this.valueInput);
-      this.$el.parentElement.appendChild(this.dropdown);
-    }
-
-    addEvents() {
-      this.dropdown.addEventListener("click", e => {
-        const target = e.target;
-        this.dropdown.classList.toggle("selecting");
-
-        // Save new value only when clicked on li
-        if (target.tagName === "LI") {
-          this.valueInput.value = target.dataset.value;
-          this.current.innerText = target.innerText;
+        init() {
+            this.createElements();
+            this.addEvents();
+            this.$el.parentElement.removeChild(this.$el);
         }
-      });
+
+        createElements() {
+            // Input for value
+            this.valueInput = document.createElement("input");
+            this.valueInput.type = "text";
+            this.valueInput.name = this.$el.name;
+
+            // Dropdown container
+            this.dropdown = document.createElement("div");
+            this.dropdown.classList.add("dropdown");
+
+            // List container
+            this.ul = document.createElement("ul");
+
+            // All list options
+            this.options.forEach((el, i) => {
+                const li = document.createElement("li");
+                li.dataset.value = el.value;
+                li.innerText = el.innerText;
+
+                if (i === 0) {
+                    // First clickable option
+                    this.current = document.createElement("div");
+                    this.current.innerText = el.innerText;
+                    this.dropdown.appendChild(this.current);
+                    this.valueInput.value = el.value;
+                    li.classList.add("selected");
+                }
+
+                this.ul.appendChild(li);
+            });
+
+            this.dropdown.appendChild(this.ul);
+            this.dropdown.appendChild(this.valueInput);
+            this.$el.parentElement.appendChild(this.dropdown);
+        }
+
+        addEvents() {
+            this.dropdown.addEventListener("click", e => {
+                const target = e.target;
+                this.dropdown.classList.toggle("selecting");
+
+                // Save new value only when clicked on li
+                if (target.tagName === "LI") {
+                    this.valueInput.value = target.dataset.value;
+                    this.current.innerText = target.innerText;
+                }
+            });
+        }
     }
-  }
-  document.querySelectorAll(".form-group--dropdown select").forEach(el => {
-    new FormSelect(el);
-  });
 
-  /**
-   * Hide elements when clicked on document
-   */
-  document.addEventListener("click", function(e) {
-    const target = e.target;
-    const tagName = target.tagName;
-
-    if (target.classList.contains("dropdown")) return false;
-
-    if (tagName === "LI" && target.parentElement.parentElement.classList.contains("dropdown")) {
-      return false;
-    }
-
-    if (tagName === "DIV" && target.parentElement.classList.contains("dropdown")) {
-      return false;
-    }
-
-    document.querySelectorAll(".form-group--dropdown .dropdown").forEach(el => {
-      el.classList.remove("selecting");
+    document.querySelectorAll(".form-group--dropdown select").forEach(el => {
+        new FormSelect(el);
     });
-  });
-
-  /**
-   * Switching between form steps
-   */
-  class FormSteps {
-    constructor(form) {
-      this.$form = form;
-      this.$next = form.querySelectorAll(".next-step");
-      this.$prev = form.querySelectorAll(".prev-step");
-      this.$step = form.querySelector(".form--steps-counter span");
-      this.currentStep = 1;
-
-      this.$stepInstructions = form.querySelectorAll(".form--steps-instructions p");
-      const $stepForms = form.querySelectorAll("form > div");
-      this.slides = [...this.$stepInstructions, ...$stepForms];
-
-      this.init();
-    }
 
     /**
-     * Init all methods
+     * Hide elements when clicked on document
      */
-    init() {
-      this.events();
-      this.updateForm();
-    }
+    document.addEventListener("click", function (e) {
+        const target = e.target;
+        const tagName = target.tagName;
 
-    /**
-     * All events that are happening in form
-     */
-    events() {
-      // Next step
-      this.$next.forEach(btn => {
-        btn.addEventListener("click", e => {
-          e.preventDefault();
-          this.currentStep++;
-          this.updateForm();
-        });
-      });
+        if (target.classList.contains("dropdown")) return false;
 
-      // Previous step
-      this.$prev.forEach(btn => {
-        btn.addEventListener("click", e => {
-          e.preventDefault();
-          this.currentStep--;
-          this.updateForm();
-        });
-      });
-
-      // Form submit
-      this.$form.querySelector("form").addEventListener("submit", e => this.submit(e));
-    }
-
-    /**
-     * Update form front-end
-     * Show next or previous section etc.
-     */
-    updateForm() {
-      this.$step.innerText = this.currentStep;
-
-      // TODO: Validation
-
-      this.slides.forEach(slide => {
-        slide.classList.remove("active");
-
-        if (slide.dataset.step == this.currentStep) {
-          slide.classList.add("active");
+        if (tagName === "LI" && target.parentElement.parentElement.classList.contains("dropdown")) {
+            return false;
         }
-      });
 
-      this.$stepInstructions[0].parentElement.parentElement.hidden = this.currentStep >= 5;
-      this.$step.parentElement.hidden = this.currentStep >= 5;
+        if (tagName === "DIV" && target.parentElement.classList.contains("dropdown")) {
+            return false;
+        }
 
-      const bagsQnt = document.getElementById("inputBagsQnt");
-      const institution = document.getElementById("inputInstitution");
-      const street = document.getElementById("inputStreet");
-      const city = document.getElementById("inputCity");
-      const zipCode = document.getElementById("inputZipCode");
-      const phoneNumber = document.getElementById("phoneNumber");
-      const pickUpDate = document.getElementById("inputPickUpDate");
-      const pickUpTime = document.getElementById("inputPickUpTime");
-      const pickUpComment = document.getElementById("inputPickUpComment");
+        document.querySelectorAll(".form-group--dropdown .dropdown").forEach(el => {
+            el.classList.remove("selecting");
+        });
+    });
 
-      bagsQnt.innerText = document.getElementById("quantity").value;
+    /**
+     * Switching between form steps
+     */
+    class FormSteps {
+        constructor(form) {
+            this.$form = form;
+            this.$next = form.querySelectorAll(".next-step");
+            this.$prev = form.querySelectorAll(".prev-step");
+            this.$step = form.querySelector(".form--steps-counter span");
+            this.currentStep = 1;
 
-      const categoriesChecked = document.querySelectorAll("[name=categories]:checked");
-      categoriesChecked.forEach(function (element) {
-        const text = element.parentElement.querySelector(".description");
-        bagsQnt.append(" " + text.innerHTML + ";");
-      })
+            this.$stepInstructions = form.querySelectorAll(".form--steps-instructions p");
+            const $stepForms = form.querySelectorAll("form > div");
+            this.slides = [...this.$stepInstructions, ...$stepForms];
 
-      const institutionChecked = document.querySelectorAll("[name=institution]:checked");
-      institutionChecked.forEach(function (element) {
-        const institutionName = element.parentElement.parentElement.querySelector(".title");
-        institution.append("\\\ " + institutionName.innerHTML + " //");
-      })
+            this.init();
+        }
 
-      street.innerText = document.getElementById("street").value;
-      city.innerText = document.getElementById("city").value;
-      zipCode.innerText = document.getElementById("zipCode").value;
-      phoneNumber.innerText = document.getElementById("telNo").value;
-      pickUpDate.innerText = document.getElementById("pickUpDate").value;
-      pickUpTime.innerText = document.getElementById("pickUpTime").value;
-      pickUpComment.innerText = document.getElementById("pickUpComment").value;
+        /**
+         * Init all methods
+         */
+        init() {
+            this.events();
+            this.updateForm();
+        }
+
+        /**
+         * All events that are happening in form
+         */
+        events() {
+            // Next step
+            this.$next.forEach(btn => {
+                btn.addEventListener("click", e => {
+                    e.preventDefault();
+                    this.currentStep++;
+                    this.updateForm();
+                });
+            });
+
+            // Previous step
+            this.$prev.forEach(btn => {
+                btn.addEventListener("click", e => {
+                    e.preventDefault();
+                    this.currentStep--;
+                    this.updateForm();
+                });
+            });
+
+            // Form submit
+            this.$form.querySelector("form").addEventListener("submit", e => this.submit(e));
+        }
+
+        /**
+         * Update form front-end
+         * Show next or previous section etc.
+         */
+        updateForm() {
+            this.$step.innerText = this.currentStep;
+
+            // TODO: Validation
+
+            this.slides.forEach(slide => {
+                slide.classList.remove("active");
+
+                if (slide.dataset.step == this.currentStep) {
+                    slide.classList.add("active");
+                }
+            });
+
+            this.$stepInstructions[0].parentElement.parentElement.hidden = this.currentStep >= 5;
+            this.$step.parentElement.hidden = this.currentStep >= 5;
+
+            if (this.currentStep >= 5) {
+                const bagsQnt = document.getElementById("inputBagsQnt");
+                const institution = document.getElementById("inputInstitution");
+                const street = document.getElementById("inputStreet");
+                const city = document.getElementById("inputCity");
+                const zipCode = document.getElementById("inputZipCode");
+                const phoneNumber = document.getElementById("phoneNumber");
+                const pickUpDate = document.getElementById("inputPickUpDate");
+                const pickUpTime = document.getElementById("inputPickUpTime");
+                const pickUpComment = document.getElementById("inputPickUpComment");
+
+                bagsQnt.innerText = document.getElementById("quantity").value;
+
+                const categoriesChecked = document.querySelectorAll("[name=categories]:checked");
+                categoriesChecked.forEach(function (element) {
+                    const text = element.parentElement.querySelector(".description");
+                    bagsQnt.append(" " + text.innerHTML + ";");
+                })
+                const institutionChecked = document.querySelector("[name=institution]:checked");
+                const institutionName = institutionChecked.parentElement.querySelector(".title");
+                institution.innerText = institutionName.innerText;
+
+                street.innerText = document.getElementById("street").value;
+                city.innerText = document.getElementById("city").value;
+                zipCode.innerText = document.getElementById("zipCode").value;
+                phoneNumber.innerText = document.getElementById("telNo").value;
+                pickUpDate.innerText = document.getElementById("pickUpDate").value;
+                pickUpTime.innerText = document.getElementById("pickUpTime").value;
+                pickUpComment.innerText = document.getElementById("pickUpComment").value;
+            }
+        }
     }
-  }
-  const form = document.querySelector(".form--steps");
-  if (form !== null) {
-    new FormSteps(form);
-  }
+
+    const form = document.querySelector(".form--steps");
+    if (form !== null) {
+        new FormSteps(form);
+    }
 });
