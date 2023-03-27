99d2c8eab8d9898b0cb34974fa635782f7d1ee23
Griegoriens <skwarnel@gmail.com>
2023-03-27 22:08:13 +0200

    Register form displaying added

--- src/main/java/pl/coderslab/charity/controller/UserController.java
+++ src/main/java/pl/coderslab/charity/controller/UserController.java
@@ -1,4 +1,14 @@
 package pl.coderslab.charity.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class UserController {
+
+    @RequestMapping("/register")
+    public String userRegister() {
+        return "register";
+    }
+
 }
